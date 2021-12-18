b23120a49100a758dadfaf41579013c710ab50c4
sharath1493 <94543949+sharath1493@users.noreply.github.com>
2021-12-18 11:33:05 +0530

    Update FrameworkConstants.java

--- src/main/java/utils/FrameworkConstants.java
+++ src/main/java/utils/FrameworkConstants.java
@@ -3,6 +3,6 @@ package utils;
 public final class FrameworkConstants {
 	
 	public static final String URL= "https://www.saucedemo.com/";
-	public static final String RUNTYPE = "LOCAL"; //LOCAL if u want to run locally
+	public static final String RUNTYPE = "REMOTE"; //LOCAL if u want to run locally
 
 }
